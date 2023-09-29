29ef71b40d7e19b90bdae9ecc480688961e3d0df
Michael Stahnke <stahnke@floxdev.com>
2023-09-29 17:53:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# hubot-the-battle
+This is a small module to output a random (curated) image about GI Joe if somebody says "knowing is half the battle".
+
+
+# License
+WTFPL
+
+
+# Inspiration
+The internal hubot that ran at Puppet, Inc for years. It needed new images since they are now 404s.
+
+# Installation
+
+
+`npm install --save hubot-the-battle`
+
+Add 'hubot-the-battle to your `external-scripts` file. That's all.
+
---
+++ src/battle.js
@@ -0,0 +1,21 @@
+// Description: Randomly sends a GI Joe knowing is half the battle image
+//
+// Commands: none
+// Dependencies: none
+// Author: stahnma
+// Configuration: none
+
+function randomKnow() {
+  var knowing = new Array();
+  knowing[0] = "https://i.imgur.com/yZqAF7e.gif"
+  knowing[1] = "https://i.imgur.com/1jcvTBp.jpg"
+  knowing[2] = "https://i.imgur.com/s88AWgH.png"
+  index = Math.floor(Math.random() * knowing.length);
+  return knowing[index];
+}
+
+module.exports = function(robot) {
+  robot.hear(/knowing is half the battle/i, function(msg) {
+    msg.send(randomKnow());
+  });
+}
